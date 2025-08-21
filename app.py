b1990258ecffe0e984ccb89181f0b8cc2bf57f6f
RahJess <jessicakantotiana@gmail.com>
2025-08-21 11:47:49 +0200

    feat: add simple endpoint /ping and configure OAS for exo1 in exo1.yaml

---
+++ app.py
@@ -0,0 +1,14 @@
+import base64
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.requests import Request
+from starlette.responses import Response, JSONResponse
+
+app = FastAPI()
+
+
+@app.get("/ping")
+def hello():
+    return Response(content="pong", status_code=200, media_type="text/plain")
